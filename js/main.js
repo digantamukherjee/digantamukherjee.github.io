835ff95a034c42df4189d1d8bad1d13e7a6ac5c0
Diganta Mukherjee <diganta.m@gmail.com>
2019-03-17 13:34:52 +0530

    Animated Dice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-window.onload = function(e){
-	
-};
-window.document.onload = function(){
 
-};
+$(document).ready(function(e){
+	
+	/*compensate for the height of fixed positioned navbar to start mainflow after its height*/
+	$(".clearHeader").height($(".navbar").outerHeight());
+	$(".contentDiv").click(function(e){
+		if($(".contentDivLayout >.child").hasClass("transform1")){
+			$(".contentDivLayout").removeClass("transform1");
+		}else{
+			$(".contentDivLayout").addClass("transform1");
+		}
+	});
+});
+
+$(window).on("load", function(){
+	
+});
